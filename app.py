47dc20515e2c7b22ef8f679f7c26533ab0e23e77
oshinj <45084634+oshinj@users.noreply.github.com>
2020-08-24 22:56:18 -0700

    initial implementation

--- app.py
+++ app.py
@@ -16,4 +16,17 @@ def displayOdd(number):
 
 @app.route('/<int:number>/prime')
 def displayPrime(number):
-    return render_template('primeNums.html', number=number)
+    primes = []
+    for num in range (1, number+1, 2):
+        if num == 1:
+            primes.append(2)
+        else:
+            prime = True
+            for divis in range(3, num//2):
+                if num % divis == 0:
+                    prime = False
+                    break
+            if prime:
+                primes.append(num)
+
+    return render_template('primeNums.html', primes=primes)
